f569a1fe9d96aeaf0bfff3bdf6fff733d1d1d6ee
CrispyXYZ <64761703+CrispyXYZ@users.noreply.github.com>
2026-03-12 23:41:58 +0800

    启用/actuator/info

--- src/main/java/io/github/crispyxyz/wangran/config/SecurityConfig.java
+++ src/main/java/io/github/crispyxyz/wangran/config/SecurityConfig.java
@@ -33,7 +33,7 @@ public class SecurityConfig {
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth.requestMatchers("/auth/**")
                                                .permitAll()
-                                               .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**")
+                                               .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**", "/actuator/health")
                                                .permitAll()
                                                .requestMatchers("/events/public")
                                                .permitAll()
