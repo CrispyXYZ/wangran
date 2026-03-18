e504e0cda0ddf5e278bd43d4032e8c2181bc2426
CrispyXYZ <64761703+CrispyXYZ@users.noreply.github.com>
2026-03-18 19:37:07 +0800

    添加/actuator/health到swagger

--- src/main/java/io/github/crispyxyz/wangran/config/SwaggerConfig.java
+++ src/main/java/io/github/crispyxyz/wangran/config/SwaggerConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -23,5 +24,22 @@ public class SwaggerConfig {
                             ))
                             .info(new Info().title("望冉购票系统 API")
                                             .description("本系统 API 遵循统一返回格式与分页格式"));
+    }
+
+    @Bean
+    public GroupedOpenApi defaultApi() {
+        return GroupedOpenApi.builder()
+                .group("default")
+                .pathsToMatch("/**")
+                .pathsToExclude("/actuator/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi actuatorApi() {
+        return GroupedOpenApi.builder()
+                .group("actuator")
+                .pathsToMatch("/actuator/health")
+                .build();
     }
 }
